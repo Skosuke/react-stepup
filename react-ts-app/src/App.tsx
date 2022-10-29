238fd2ca950517184d2c8af88d58fc4aa371aa0e
Skosuke <38691856+Skosuke@users.noreply.github.com>
2022-10-29 16:14:57 +0900

    stydy1

--- react-ts-app/src/App.tsx
+++ react-ts-app/src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Practice1 } from './practices/practice1';
+import { Practice2 } from './practices/practice2';
+import { Practice3 } from './practices/practice3';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Practice1 />
+      <Practice2 />
+      <Practice3 />
     </div>
   );
 }
